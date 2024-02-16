87922e2f609ac10cbc3427f4a3383143c4d32c11
Ahmad-Samandar <shoaib.samandar@outlook.com>
2024-02-16 11:40:17 -0500

    used getCurrentUrl() and getTitle() methods in a test and quit the driver

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,7 +11,18 @@ public class Main {
        // Chrome - ChromeDriver --> Methods
         WebDriver driver = new ChromeDriver();
        // WebDriver --> Interface --> Methods like get, close .. etc.
-        driver.close();
 
+        // Navigate into a website using get()
+        driver.get("https://www.google.com");
+
+        // get the title of the tab using getTitle() --> Returns void
+        System.out.println(driver.getTitle());
+
+        // To check and get the current URL --> Returns void
+        System.out.println(driver.getCurrentUrl());
+
+        // You can close the current window using close()
+        driver.close();
+        driver.quit();
     }
 }

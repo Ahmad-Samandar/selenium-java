67d88dd00a414dc28807e613f0f360233c3b4524
Ahmad-Samandar <Ahmad.samandar@outlook.com>
2024-02-15 22:40:28 -0500

    Initialized ChromeDriver using WebDriver Interface

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,17 @@
 package org.example;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+       // Chrome - ChromeDriver --> Methods
+        WebDriver driver = new ChromeDriver();
+       // WebDriver --> Interface --> Methods like get, close .. etc.
+        driver.close();
+
     }
 }

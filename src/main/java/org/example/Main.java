d76e437d8c10e4857a9f1d1cd417ef155c019c6d
Ahmad-Samandar <Ahmad.samandar@outlook.com>
2024-06-13 10:16:24 -0400

    Updated Code on Main Class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,15 +3,15 @@ package org.example;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        runSeleniumTest();
+    }
 
-       // Chrome - ChromeDriver --> Methods
+    public static void runSeleniumTest() {
+        // Chrome - ChromeDriver --> Methods
         WebDriver driver = new ChromeDriver();
-       // WebDriver --> Interface --> Methods like get, close .. etc.
+        // WebDriver --> Interface --> Methods like get, close .. etc.
 
         // Navigate into a website using get()
         driver.get("https://www.google.com");
